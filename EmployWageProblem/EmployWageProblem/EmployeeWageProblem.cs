9e2b6767c10ba2d4ba5eb8b859302c59dfea25eb
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-09 20:17:23 +0530

    [gunasekhar9] add : Working of Employee Wage is Created

--- EmployWageProblem/EmployWageProblem/EmployeeWageProblem.cs
+++ EmployWageProblem/EmployWageProblem/EmployeeWageProblem.cs
@@ -11,18 +11,18 @@ namespace EmployeeWageProblem
         public void DailyEmployeeWage()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 3);// 0,2
-            switch (empCheck)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0; ;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage : " + empWage);
