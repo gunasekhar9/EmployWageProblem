2656a194beab312fbbee60964ab13c1e5e6088ca
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-09 20:06:15 +0530

    [gunasekhar9] add : Daily Employee Wage Is Created

--- EmployWageProblem/EmployWageProblem/EmployeeWageProblem.cs
+++ EmployWageProblem/EmployWageProblem/EmployeeWageProblem.cs
@@ -6,23 +6,19 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWageProblem
     {
-        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
+        public const int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
         int empHrs = 0, empWage = 0;
         public void DailyEmployeeWage()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 3);// 0,2
-            switch (empCheck)
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0; ;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage : " + empWage);
