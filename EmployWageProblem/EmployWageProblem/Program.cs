9b9e90a818d4133c7152d92897d740fb78eb9d05
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-16 16:10:37 +0530

    [gunasekhar9] add : Total EmplyeeWage for Companies is Calculated

--- EmployWageProblem/EmployWageProblem/Program.cs
+++ EmployWageProblem/EmployWageProblem/Program.cs
@@ -7,8 +7,12 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Computation Problem");
-            EmployeeWageProblem employee = new EmployeeWageProblem();
-            employee.MonthlyEmployeeWage();
+            EmpWageBuilder RelianceTrends = new EmpWageBuilder("RelianceTrends", 21, 25, 100);
+            EmpWageBuilder AMBMall = new EmpWageBuilder("AMBMall", 27, 26, 160);
+            RelianceTrends.computeEmpWage();
+            Console.WriteLine(RelianceTrends.toString());
+            AMBMall.computeEmpWage();
+            Console.WriteLine(AMBMall.toString());
         }
     }
 }
