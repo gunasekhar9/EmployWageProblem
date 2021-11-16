dc1f0942d8ebe0251f68ab98d85646bc957a7e06
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-16 16:45:08 +0530

    [gunasekhar9] add : Manage Employee Wage is Done

--- EmployWageProblem/EmployWageProblem/Program.cs
+++ EmployWageProblem/EmployWageProblem/Program.cs
@@ -7,12 +7,11 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Computation Problem");
-            EmpWageBuilder RelianceTrends = new EmpWageBuilder("RelianceTrends", 21, 25, 100);
-            EmpWageBuilder AMBMall = new EmpWageBuilder("AMBMall", 27, 26, 160);
-            RelianceTrends.computeEmpWage();
-            Console.WriteLine(RelianceTrends.toString());
-            AMBMall.computeEmpWage();
-            Console.WriteLine(AMBMall.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("RelianceTrends", 21, 25, 123);
+            empWageBuilder.addCompanyEmpWage("AMBMall", 28, 26, 160);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
+
